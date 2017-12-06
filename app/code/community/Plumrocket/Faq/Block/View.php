0d542d2cdb53d520eb19a9fdd7ebd6f9a4f39129
Gladii <gladii.ii22@gmail.com>
2017-12-06 20:46:18 +0200

    change block

--- app/code/community/Plumrocket/Faq/Block/View.php
+++ app/code/community/Plumrocket/Faq/Block/View.php
@@ -2,12 +2,12 @@
 
 class Plumrocket_Faq_Block_View extends Mage_Core_Block_Template
 {
-	public function viewAction() 
+	public function getItem() 
 	{
-
-		$this->getLayout()->getBlock('faqs.content')->assign(array(
-				"faqsItem" => $faqs,
-			));
-
+		$faqsId = Mage::app()->getRequest()->getParam('id', 0);
+		$faqs = Mage::getModel('plumrocketfaq/faqs')->load($faqsId);
+		
+		return $faqs;
+		
 	}
 }	
--- app/code/community/Plumrocket/Faq/controllers/IndexController.php
+++ app/code/community/Plumrocket/Faq/controllers/IndexController.php
@@ -10,20 +10,7 @@ class Plumrocket_Faq_IndexController extends Mage_Core_Controller_Front_Action
 
     public function viewAction()
     {
-		$faqsId = Mage::app()->getRequest()->getParam('id', 0);
-		$faqs = Mage::getModel('plumrocketfaq/faqs')->load($faqsId);
-		
-
-        if ($faqs->getId() > 0) {
-            $this->loadLayout();
-            
-			/*$this->getLayout()->getBlock('faqs.content')->assign(array(
-                "faqsItem" => $faqs,
-            ));*/
-            $this->renderLayout();
-        } else {
-            $this->_forward('noRoute');
-        }
-
+		$this->loadLayout();
+		$this->renderLayout();
     }
 }
