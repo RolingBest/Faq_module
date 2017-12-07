420effdfd72967b82a01d22633b268c7ad6710d7
Gladii <gladii.ii22@gmail.com>
2017-12-07 19:13:56 +0200

    add enable/disable module

--- app/code/community/Plumrocket/Faq/Helper/Data.php
+++ app/code/community/Plumrocket/Faq/Helper/Data.php
@@ -2,5 +2,19 @@
 
 class Plumrocket_Faq_Helper_Data extends Mage_Core_Helper_Abstract
 {
+	public function __construct() 
+	{
+		$resource = Mage::getSingleton('core/resource');
+		$writeConnection = $resource->getConnection('core_write');
 	
+		if (Mage::getStoreConfig('faq_section/settings/enable')) {
+			$query = "UPDATE `core_config_data` SET `value` = 0 
+					WHERE `core_config_data`.`path` LIKE '%Plumrocket_Faq%';";
+		} else {
+			$query = "UPDATE `core_config_data` SET `value` = 1 
+					WHERE `core_config_data`.`path` LIKE '%Plumrocket_Faq%';";
+		}
+		
+		$writeConnection->query($query);
+	}
 }
