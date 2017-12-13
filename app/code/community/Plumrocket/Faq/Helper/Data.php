d3e867fc6ec475bdd8c5c2e5e9c6c3c4d03d69c5
Gladii <gladii.ii22@gmail.com>
2017-12-13 23:31:50 +0200

    add disable frontend

--- app/code/community/Plumrocket/Faq/Helper/Data.php
+++ app/code/community/Plumrocket/Faq/Helper/Data.php
@@ -2,19 +2,9 @@
 
 class Plumrocket_Faq_Helper_Data extends Mage_Core_Helper_Abstract
 {
-	public function __construct() 
-	{
-		$resource = Mage::getSingleton('core/resource');
-		$writeConnection = $resource->getConnection('core_write');
-	
-		if (Mage::getStoreConfig('faq_section/settings/enable')) {
-			$query = "UPDATE `core_config_data` SET `value` = 0 
-					WHERE `core_config_data`.`path` LIKE '%Plumrocket_Faq%';";
-		} else {
-			$query = "UPDATE `core_config_data` SET `value` = 1 
-					WHERE `core_config_data`.`path` LIKE '%Plumrocket_Faq%';";
-		}
-		
-		$writeConnection->query($query);
-	}
-}
+	public function moduleEnabled()
+    {
+        return (bool)Mage::getStoreConfig('faq_section/settings/enable');
+    }
+
+}	
